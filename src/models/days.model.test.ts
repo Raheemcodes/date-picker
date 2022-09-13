004b48fd476ed3d0be58ce3a4ac8a55ed8eff888
draco-se <73908519+draco-se@users.noreply.github.com>
2022-09-13 20:56:38 +0100

    testing

--- src/models/days.model.test.ts
+++ src/models/days.model.test.ts
@@ -1,4 +1,4 @@
-import { getWeekDay } from './days.model';
+import { getWeekDay, firstWkDay } from './days.model';
 
 describe('getWeekDay()', () => {
   it('should return week day', () => {
@@ -21,4 +21,30 @@ describe('getWeekDay()', () => {
     expect(result1).toEqual(result2);
     expect(result3).toEqual(result4);
   });
+});
+
+describe('firstWkDay()', () => {
+  it('should return week day of the month', () => {
+    const year: number = 2022;
+    const month: number = 9;
+
+    const result: number = firstWkDay(year, month - 1);
+
+    expect(result).toBe(4);
+  });
+
+  it('should return same week day for year < hundred and year > 2000', () => {
+    const result1: number = firstWkDay(1, 1);
+    const result2: number = firstWkDay(2001, 1);
+
+    const result3: number = firstWkDay(99, 1);
+    const result4: number = firstWkDay(2099, 1);
+
+    expect(result1).toEqual(result2);
+    expect(result3).toEqual(result4);
+  });
+});
+
+describe('getDays()', () => {
+  //
 });
